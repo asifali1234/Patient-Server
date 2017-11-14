e32b13d9fd2c8877c55dc84c4a802c7038c7be86
aravi123 <aravindcranjan54@gmail.com>
2017-11-15 03:14:41 +0530

    field change

--- service/booking.js
+++ service/booking.js
@@ -2,14 +2,14 @@
  * Created by aravind on 11/11/17.
  */
 
-let bookforDoctor = (db,userid,doctorid,date)=>{
+let bookforDoctor = (db,userid,doctorid,date,doctorName)=>{
 	let currdate = new Date().getDate()+""+new Date().getMonth()+""+new Date().getFullYear();
 	console.log(currdate);
 	return new Promise((resolve,reject)=>{
 		db.token.findAndModify({query:{doctorid:doctorid,date:date},update:{$inc:{tokenno:1}},upsert:true,new:true},(err,docs)=>{
 			if(!err){
 				let token =docs.tokenno;
-				db.appointements.save({googleid:userid,doctorid:doctorid,date:date,tokenno:token},(err)=>{
+				db.appointements.save({googleid:userid,doctorid:doctorid,date:date,tokenno:token,doctorName:doctorName},(err)=>{
 					if(!err){
 						resolve(docs);
 					}
