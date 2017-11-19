705c80786362a82d14abaf1f639c50c370fff88c
Aravind C Ranjan <aravindcranjan54@gmail.com>
2017-11-19 19:01:35 +0530

    doctordetails api changed

--- service/doctordetails.js
+++ service/doctordetails.js
@@ -1,12 +1,20 @@
 /**
  * Created by aravind on 11/11/17.
  */
+
+let tokenno = require('./gettokenNos.js');
+
 let doctordetails = (db)=>{
 
 	return new Promise((resolve,reject)=>{
-		db.doctors.find((err,docs)=>{
+		db.doctors.find((err,doctors)=>{
 			if(!err){
-				resolve(docs);
+			    //console.log(doctors);
+				tokenno.gettokenno(db,doctors).then((docs)=>{
+				    resolve(docs);
+                }).catch((err)=>{
+				    reject(err);
+                });
 			}
 			else{
 				reject(err);
